da1ba2aade0d07b4287cd2405e0ec1ac48e973ad
skkim125 <kthanks125@gmail.com>
2024-11-01 14:32:22 +0900

    [Add] UIView+ 생성
    - UIView extension 파일 UIView+ 생성
    - DMView의 collectionView Layout 코드 UIView+로 이동

---
+++ Amor/Extension/UIView+.swift
@@ -0,0 +1,58 @@
+//
+//  UIView+.swift
+//  Amor
+//
+//  Created by 김상규 on 11/1/24.
+//
+
+import UIKit
+
+// MARK: DMView+
+extension UIView {
+    func setDmCollectionViewLayout(_ type: DMCollectionViewType) -> UICollectionViewLayout {
+        
+        let layout: UICollectionViewLayout
+        
+        switch type {
+        case .user:
+            layout = setDMUserCollectionViewLayout()
+        case .chat:
+            layout = setDMChatCollectionViewLayout()
+        }
+        
+        return layout
+    }
+    
+    private func setDMUserCollectionViewLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
+        )
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(80),
+            heightDimension: .fractionalHeight(1)
+        )
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = .init(top: 0, leading: 5, bottom: 0, trailing: 5)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    private func setDMChatCollectionViewLayout() -> UICollectionViewLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .plain)
+        listConfiguration.showsSeparators = false
+        
+        let layout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+        
+        return layout
+    }
+}
